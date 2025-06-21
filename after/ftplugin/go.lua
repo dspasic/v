dcc5e085e8b9dfc6c53c167fbbd43692f02cfe36
Dejan Spasic <dejan.spasic@bitweise.biz>
2025-06-21 18:55:22 +0200

    Go Lang settings
    Adjusted tabstop and co for go lang.

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -4,3 +4,8 @@ function go_mod_tidy()
 end
 
 vim.api.nvim_create_user_command("GoTidyMod", go_mod_tidy, {})
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
